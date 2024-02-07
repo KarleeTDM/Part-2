7dcb4eb14bd35369fc20d1c1ef8720893c41ce1e
mackkarl <mackkarl@OA-S141-10>
2024-02-07 15:06:02 -0500

    Completed Task 2.

--- Assets/Week 5/Scripts/Knight.cs
+++ Assets/Week 5/Scripts/Knight.cs
@@ -13,6 +13,7 @@ public class Knight : MonoBehaviour
     public float health;
     public float maxHealth = 5;
     bool isDead = false;
+    public HealthBar healthBar;
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
@@ -46,6 +47,7 @@ public class Knight : MonoBehaviour
         if (isDead) return;
         clickingOnSelf = true;
         TakeDamage(1);
+        healthBar.TakeDamage(1);
     }
 
     private void OnMouseUp()
@@ -53,7 +55,7 @@ public class Knight : MonoBehaviour
         clickingOnSelf = false;
     }
 
-    void TakeDamage(float damage)
+    public void TakeDamage(float damage)
     {
         health -= damage;
         health = Mathf.Clamp(health, 0, maxHealth);
