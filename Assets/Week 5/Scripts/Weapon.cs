8ffd50c5705ec9f06029cfec77a60b7bd7fb550d
LAPTOP-EDOG7L2U\Lenovo <karlee100mackenzie@gmail.com>
2024-02-07 20:10:53 -0500

    Made prgress on getting the arrow to move. Unknown if the colliders work.

--- Assets/Week 5/Scripts/Weapon.cs
+++ Assets/Week 5/Scripts/Weapon.cs
@@ -5,14 +5,21 @@ using UnityEngine;
 public class Weapon : MonoBehaviour
 {
     Rigidbody2D rb;
-    float speed = 1.0f;
+    public float speed = 10f;
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
     }
 
-    void Update()
+    private void FixedUpdate()
     {
-        
+        Vector2 direction = new Vector2(speed * Time.deltaTime, 0);
+        rb.MovePosition(rb.position - direction);
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        SendMessage("TakeDamage", 1);
+        Destroy(gameObject);
     }
 }
