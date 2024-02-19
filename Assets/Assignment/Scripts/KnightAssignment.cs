6dc0978fefe76660a37c36c3d5c85c3b87509d62
LAPTOP-EDOG7L2U\Lenovo <karlee100mackenzie@gmail.com>
2024-02-19 13:41:48 -0500

    Added a lerp and animation curve! :D

---
+++ Assets/Assignment/Scripts/KnightAssignment.cs
@@ -0,0 +1,107 @@
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KnightAssignment : MonoBehaviour
+{
+    Rigidbody2D rb;
+    Animator animator;
+    Vector2 destination;
+    Vector2 movement;
+    public float speed = 3;
+    bool clickingOnSelf = false;
+    public int health;
+    public int maxHealth = 5;
+    bool isDead = false;
+    public HealthBar healthBar;
+    public AnimationCurve dead;
+    float deathTimer;
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
+        health = PlayerPrefs.GetInt("KnightHealth", 5);
+        SendMessage("SetInitialHealth", health);
+    }
+
+    private void FixedUpdate()
+    {
+        if (isDead)
+        {
+            deathTimer += 0.1f * Time.deltaTime;
+            float timing = dead.Evaluate(deathTimer);
+            if (transform.localScale.z < 0f)
+            {
+                return;
+            }
+            transform.localScale = Vector3.Lerp(transform.localScale, Vector3.zero, timing);
+        }
+        if (!isDead)
+        {
+            deathTimer = 0;
+            movement = destination - (Vector2)transform.position;
+            if (movement.magnitude < 0.1)
+            {
+                movement = Vector2.zero;
+            }
+            rb.MovePosition(rb.position + movement.normalized * speed * Time.deltaTime);
+        }   
+    }
+
+    void Update()
+    {
+        if (isDead) return;
+        if (Input.GetMouseButtonDown(0) && !clickingOnSelf && !EventSystem.current.IsPointerOverGameObject())
+        {
+            destination = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        }
+        animator.SetFloat("Movement", movement.magnitude);
+        if (Input.GetMouseButtonDown(1))
+        {
+            animator.SetTrigger("Attack");
+        }
+    }
+
+    private void OnMouseDown()
+    {
+        if (isDead) return;
+        clickingOnSelf = true;
+        SendMessage("TakeDamage", 1);
+    }
+
+    private void OnMouseUp()
+    {
+        clickingOnSelf = false;
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        SendMessage("TakeDamage", 1);
+    }
+
+    public void TakeDamage(int damage)
+    {
+        health -= damage;
+        health = Mathf.Clamp(health, 0, maxHealth);
+        PlayerPrefs.SetInt("KnightHealth", health);
+        if (health <= 0)
+        {
+            isDead = true;
+            animator.SetTrigger("Death");
+        }
+        else
+        {
+            isDead = false;
+            animator.SetTrigger("TakeDamage");
+            transform.localScale = Vector3.one;
+            destination = Vector2.zero;
+        }
+    }
+
+    public void SetInitialHealth(int HP)
+    {
+        health = HP;
+    }
+}
