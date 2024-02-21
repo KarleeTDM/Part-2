a836bfbe75301cf9b3592ad7c8334e1fe9593f95
mackkarl <mackkarl@OA-S141-11>
2024-02-21 14:18:34 -0500

    Completed Task 1! :D

---
+++ Assets/Week 7/Scripts/Player.cs
@@ -0,0 +1,32 @@
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    SpriteRenderer sr;
+    public Color selectedColour;
+    public Color unselectedColour;
+    private void Start()
+    {
+        sr = GetComponent<SpriteRenderer>();
+        Selected(false);
+    }
+    private void OnMouseDown()
+    {
+        Selected(true);
+    }
+    public void Selected(bool isSelected)
+    {
+        if (isSelected)
+        {
+            sr.color = selectedColour;
+        }
+        else
+        {
+            sr.color = unselectedColour;
+        }
+    }
+}
